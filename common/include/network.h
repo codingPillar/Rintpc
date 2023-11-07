aa4e9c08d10fd950ac08bb4adaefa9dbf7f885b3
codingPillar <adam.el-houssami@polymtl.ca>
2023-11-07 14:57:27 -0500

    added master node constant nodeAdress struct

--- common/include/network.h
+++ common/include/network.h
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <string>
 
+#include "constants.h"
 namespace rintpc {
 
 /* VALUES ARE IN NETWORK FORMAT */
@@ -12,6 +13,8 @@ struct NodeAddress{
     uint32_t ip;
     uint16_t port;
 };
+
+extern const struct NodeAddress MASTER_NODE_ADDRESS;
 
 std::string formatIP(uint32_t ip, bool networkFormat = true);
 
--- common/src/network.cpp
+++ common/src/network.cpp
@@ -1,10 +1,15 @@
 #include <network.h>
 
-std::string rintpc::formatIP(uint32_t ip, bool networkFormat){
+using namespace rintpc;
+using namespace std;
+
+extern const struct NodeAddress MASTER_NODE_ADDRESS = {.ip = htonl(MASTER_IP), .port = htons(MASTER_PORT)};
+
+string formatIP(uint32_t ip, bool networkFormat){
     char buffer[16];
     unsigned int counter = 0;
     if(networkFormat) ip = ntohl(ip);
     for(unsigned int i = 0; i < sizeof(uint32_t); i++)
-        counter += std::sprintf(&buffer[counter], (i < sizeof(uint32_t) - 1) ? "%d." : "%d", (ip >> (8 * (sizeof(uint32_t) - i - 1))) & 0x00FF);
-    return std::string(buffer);
+        counter += sprintf(&buffer[counter], (i < sizeof(uint32_t) - 1) ? "%d." : "%d", (ip >> (8 * (sizeof(uint32_t) - i - 1))) & 0x00FF);
+    return string(buffer);
 }
