ac156aea381532418c96e03a80d5de668dcc6310
codingPillar <adam.el-houssami@polymtl.ca>
2023-11-07 15:00:05 -0500

    added namespace to resolve function when linking

--- common/src/network.cpp
+++ common/src/network.cpp
@@ -1,7 +1,8 @@
 #include <network.h>
 
-using namespace rintpc;
 using namespace std;
+
+namespace rintpc {
 
 extern const struct NodeAddress MASTER_NODE_ADDRESS = {.ip = htonl(MASTER_IP), .port = htons(MASTER_PORT)};
 
@@ -12,4 +13,6 @@ string formatIP(uint32_t ip, bool networkFormat){
     for(unsigned int i = 0; i < sizeof(uint32_t); i++)
         counter += sprintf(&buffer[counter], (i < sizeof(uint32_t) - 1) ? "%d." : "%d", (ip >> (8 * (sizeof(uint32_t) - i - 1))) & 0x00FF);
     return string(buffer);
+}
+
 }
